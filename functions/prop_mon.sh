3fc5410dcbb2b67a7d3859cad8f5257ec7784354
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2024-10-20 10:55:31 +0100

    update

--- functions/prop_mon.sh
+++ functions/prop_mon.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-### check first if the utilities are installed
-#!/bin/bash
 
 # Function to check and install a package
 check_and_install() {
@@ -11,12 +9,12 @@ check_and_install() {
 # List of packages to install
 packages=(
     fetchmail
-	procmail
-	msmtp
-	msmtp-mta
-	mailutils
-	mpack
-	ca-certificates
+    procmail
+    msmtp
+    msmtp-mta
+    mailutils
+    mpack
+    ca-certificates
 )
 
 # Loop through the packages and install if not already installed
@@ -26,3 +24,47 @@ done
 
 echo "All specified packages are installed."
 
+# Ensure START_DAEMON=yes is set in /etc/default/fetchmail
+sed -i '/START_DAEMON=/ c\START_DAEMON=yes' /etc/default/fetchmail || echo "START_DAEMON=yes" >> /etc/default/fetchmail
+# Update configurations in /etc/fetchmailrc
+sed -i '/^set daemon/c\set daemon 300' /etc/fetchmailrc
+sed -i '/^set syslog/c\set syslog' /etc/fetchmailrc
+sed -i '/^set postmaster/c\set postmaster root' /etc/fetchmailrc
+sed -i '/^set no bouncemail/c\set no bouncemail' /etc/fetchmailrc
+
+# Ensure these lines exist, add if not present
+grep -q '^set daemon' /etc/fetchmailrc || echo 'set daemon 300' >> /etc/fetchmailrc
+grep -q '^set syslog' /etc/fetchmailrc || echo 'set syslog' >> /etc/fetchmailrc
+grep -q '^set postmaster' /etc/fetchmailrc || echo 'set postmaster root' >> /etc/fetchmailrc
+grep -q '^set no bouncemail' /etc/fetchmailrc || echo 'set no bouncemail' >> /etc/fetchmailrc
+# Check and add configurations to /etc/fetchmailrc if they don't exist
+if ! grep -q "# Hosts to pool" /etc/fetchmailrc; then
+    cat << EOF >> /etc/fetchmailrc
+#############################################################
+# Hosts to pool
+#############################################################
+
+# Defaults ==============================================
+# Set antispam to -1, since it is far safer to use that together with
+# no bouncemail
+
+defaults:
+timeout 300
+antispam -1
+batchlimit 100
+
+poll imap.gmail.com
+protocol IMAP
+user "******@gmail.com" is svxlink
+password "********"
+folder 'INBOX'
+# fetchlimit 1
+fetchall
+# keep
+ssl
+mda "/usr/bin/procmail"
+EOF
+fi
+
+# Set correct permissions for fetchmailrc
+chmod 600 /etc/fetchmailrc
